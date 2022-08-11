506a820788f7522cbdb45519fdba732b0a556020
abdelbassetbelaid3 <belaidabdelbasset3@gmail.com>
2022-08-11 20:33:13 +0100

    /

---
+++ main.py
@@ -0,0 +1,13 @@
+from wordhoard import Synonyms
+
+synonym = Synonyms(search_string='mother')
+synonym_results = synonym.find_synonyms()
+print(synonym_results)
+['ancestor', 'biological mother', 'birth mother', 'child-bearer', 'creator',
+ 'dam', 'female parent', 'forebearer', 'foster mother', 'ma', 'mama', 'mamma',
+ 'mammy', 'mater', 'mom', 'momma', 'mommy', 'mother-in-law', 'mum', 'mummy',
+ 'old lady', 'old woman', 'origin', 'para i', 'parent', 'predecessor',
+ 'primipara', 'procreator', 'progenitor', 'puerpera', 'quadripara',
+ 'quintipara', 'source', 'supermom', 'surrogate mother']
+git config - -global user.email belaidabdelbasset3@gmail.com
+git config - -global user.name abdelbassetbelaid3
